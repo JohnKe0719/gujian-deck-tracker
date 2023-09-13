cf1d98c7f7a65d5303e266c7592dc6042a9f32d5
JohnKe0719 <IamJohn0329@gmail.com>
2023-09-13 09:40:55 +0800

    上傳nuxt build出來的資料夾

---
+++ .nuxt/router.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _057d36ca = () => interopDefault(import('..\\pages\\deck.vue' /* webpackChunkName: "pages/deck" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/deck",
+    component: _057d36ca,
+    name: "deck___en"
+  }, {
+    path: "/cn/deck",
+    component: _057d36ca,
+    name: "deck___cn"
+  }, {
+    path: "/tw/deck",
+    component: _057d36ca,
+    name: "deck___tw"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
