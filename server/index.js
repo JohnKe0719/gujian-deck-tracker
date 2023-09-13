a546e233adb5cf561b2a2caba4e7f81b3299af88
JohnKe0719 <IamJohn0329@gmail.com>
2023-09-13 09:24:50 +0800

    將host改為'0.0.0.0'

--- server/index.js
+++ server/index.js
@@ -24,9 +24,9 @@ async function start() {
     app.use(nuxt.render)
 
     // Listen the server
-    app.listen(process.env.PORT || 5000, host)
+    app.listen(process.env.PORT || port, '0.0.0.0')
     consola.ready({
-        message: `Server listening on http://${host}:${process.env.PORT || 5000}`,
+        message: `Server listening on http://${'0.0.0.0'}:${process.env.PORT || port}`,
         badge: true
     })
 }
